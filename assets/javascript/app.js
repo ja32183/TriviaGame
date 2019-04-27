1e57f3be28b44c87dccd1ed5748252e8851232cb
Jeff Amlani <ja32183@gmail.com>
2019-04-27 10:23:14 -0400

    added files

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+
+var game= $("form");
+
+var questions = [
+{
+    question: "What year did Star Wars release?",
+    answers: ["1980", "1977", "1973", "1970"],
+    correctAnswer: "1977"
+},
+
+{
+    question: "Who played Luke Skywalker?",
+    answers: ["Darth Vader", "George Lucas", "Mark Hammil", "Harrison Ford"],
+    correctAnswer: "Mark Hammil"
+},
+
+{
+    question: "What race is Chewbacca?",
+    answers: ["Han Solo", "Minotaur", "Wookie", "Tyranosaurus"],
+    correctAnswer: "Wookie"
+},
+
+{
+    question: "Who is Lukes father?",
+    answers: ["Palpatine", "Darth Vader", "Anakin Skywalker", "Yoda"],
+    correctAnswer: "Anakin Skywalker"
+},
